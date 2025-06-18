6a3eb4a410aa1ffa82e94ce75b073183fe3a3587
baviya-23 <baviyavarshiniv@gmail.com>
2025-06-18 11:31:26 +0530

    Create day1_variables.js
    add day1 javascript code:variables and datatypes

---
+++ day1_variables.js
@@ -0,0 +1,7 @@
+let yourName = "Baviya Varshini";
+const yourAge = 21;
+let isFreelancer = true;
+
+console.log("My name is", yourName);
+console.log("My age is", yourAge);
+console.log("Am I a freelancer?", isFreelancer);
