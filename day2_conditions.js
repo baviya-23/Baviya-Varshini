68acb4ae78343f122eaba940d3f95eab0812d1a3
baviya-23 <baviyavarshiniv@gmail.com>
2025-06-19 16:27:15 +0530

    Create day2_conditions.js

---
+++ day2_conditions.js
@@ -0,0 +1,32 @@
+\\Grading system
+let score=85
+
+if(score>=90)
+{
+    console.log("Grade:A+");
+}
+else if(score>=80)
+{
+    console.log("Grade:A");
+}
+else if(score>=70)
+{
+    console.log("Grade:B");
+}
+else
+{
+    console.log("Fail");
+}
+
+\\login system
+let username="baviya";
+let password="1234";
+
+if(username=="baviya" && password=="1234")
+{
+    console.log("Login successful");
+}
+else
+{
+    console.log("Login failed")
+}
